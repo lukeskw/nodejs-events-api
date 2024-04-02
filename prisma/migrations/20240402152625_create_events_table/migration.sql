d5e4c28874b5161346174996e98c9aacc9eb8e37
lukeskw <lucasporfirioa@gmail.com>
2024-04-02 13:09:56 -0300

    build: installing prisma and configuring events table

---
+++ prisma/migrations/20240402152625_create_events_table/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "events" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "details" TEXT,
+    "slug" TEXT NOT NULL,
+    "maximum_attendees" INTEGER,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "events_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "events_slug_key" ON "events"("slug");
