4b1a2643bf114b4f6852449f4803dd47fa476077
lukeskw <lucasporfirioa@gmail.com>
2024-04-03 21:07:22 -0300

    feat: improving error handling

---
+++ src/error-handler.ts
@@ -0,0 +1,24 @@
+import { FastifyInstance } from 'fastify'
+import { ZodError } from 'zod'
+import { BadRequest } from './exceptions/bad-request.exception'
+
+type FastifyErrorHandler = FastifyInstance['errorHandler']
+
+export const errorHandler: FastifyErrorHandler = (error, request, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({
+      message: 'Error',
+      errors: error.flatten().fieldErrors,
+    })
+  }
+
+  if (error instanceof BadRequest) {
+    return reply.status(400).send({
+      message: error.message,
+    })
+  }
+
+  return reply.status(500).send({
+    message: 'Internal server error',
+  })
+}
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,48 @@
 import fastify from 'fastify'
+
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUI from '@fastify/swagger-ui'
+
+import fastifyCors from '@fastify/cors'
+
 import { registerEventsRoutes } from './events.routes'
 import {
   serializerCompiler,
   validatorCompiler,
+  jsonSchemaTransform,
 } from 'fastify-type-provider-zod'
+import { errorHandler } from './error-handler'
 
 const app = fastify()
+
+app.register(fastifyCors, {
+  origin: '*',
+})
+
+app.register(fastifySwagger, {
+  swagger: {
+    consumes: ['application/json'],
+    produces: ['application/json'],
+    info: {
+      title: 'pass.in',
+      description: 'API specs for the backend of the app pass.in',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUI, {
+  routePrefix: '/docs',
+})
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 
 registerEventsRoutes(app)
 
-app.listen({ port: 3333 }, (err, address) => {
+app.setErrorHandler(errorHandler)
+app.listen({ port: 3333, host: '0.0.0.0' }, (err, address) => {
   if (err) {
     app.log.error(err)
     process.exit(1)
