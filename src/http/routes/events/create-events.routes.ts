9d6b728a5bfe819d71e1b98650cb42ea3170dd94
lukeskw <lucasporfirioa@gmail.com>
2024-04-03 21:10:18 -0300

    feat: creating new event routes

--- src/http/routes/events/create-events.routes.ts
+++ src/http/routes/events/create-events.routes.ts
@@ -1,15 +1,17 @@
 import { FastifyInstance } from 'fastify'
-import { z, ZodError } from 'zod'
-import { fromZodError } from 'zod-validation-error'
+import { z } from 'zod'
 import { prisma } from '../../../lib/prisma'
 import { createSlugFromTitle } from '../../../utils/slug'
 import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { BadRequest } from '../../../exceptions/bad-request.exception'
 
 export const createEvents = async (app: FastifyInstance) => {
   app.withTypeProvider<ZodTypeProvider>().post(
     '/events',
     {
       schema: {
+        summary: 'Create an Event',
+        tags: ['events'],
         body: z.object({
           title: z.string().min(5),
           details: z.string().nullable(),
@@ -19,49 +21,32 @@ export const createEvents = async (app: FastifyInstance) => {
           201: z.object({
             eventId: z.string().uuid(),
           }),
-          400: z.object({
-            message: z.string(),
-          }),
-          422: z.object({
-            error: z.string(),
-          }),
         },
       },
     },
     async (request, reply) => {
-      try {
-        const { title, details, maximumAttendees } = request.body
+      const { title, details, maximumAttendees } = request.body
 
-        const slug = createSlugFromTitle(title)
+      const slug = createSlugFromTitle(title)
 
-        const eventWithSameSlug = await prisma.event.findUnique({
-          where: { slug },
-        })
+      const eventWithSameSlug = await prisma.event.findUnique({
+        where: { slug },
+      })
 
-        if (eventWithSameSlug !== null) {
-          return reply.status(400).send({
-            message: 'Event with this slug already exists',
-          })
-        }
-
-        const event = await prisma.event.create({
-          data: {
-            title,
-            details,
-            maximumAttendees,
-            slug,
-          },
-        })
-
-        return reply.status(201).send({ eventId: event.id })
-      } catch (err) {
-        if (err instanceof ZodError) {
-          const validationError = fromZodError(err)
-          console.error('Error parsing request body:', validationError.message)
-          return reply.status(422).send({ error: validationError.message })
-        }
-        console.error(err)
+      if (eventWithSameSlug !== null) {
+        throw new BadRequest('Event with this slug already exists')
       }
+
+      const event = await prisma.event.create({
+        data: {
+          title,
+          details,
+          maximumAttendees,
+          slug,
+        },
+      })
+
+      return reply.status(201).send({ eventId: event.id })
     },
   )
 }
--- src/http/routes/events/get-event.routes.ts
+++ src/http/routes/events/get-event.routes.ts
@@ -1,14 +1,16 @@
 import { FastifyInstance } from 'fastify'
-import { z, ZodError } from 'zod'
-import { fromZodError } from 'zod-validation-error'
+import { z } from 'zod'
 import { prisma } from '../../../lib/prisma'
 import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { BadRequest } from '../../../exceptions/bad-request.exception'
 
 export const getEvent = async (app: FastifyInstance) => {
   app.withTypeProvider<ZodTypeProvider>().get(
     '/events/:eventId',
     {
       schema: {
+        summary: 'Get an Event',
+        tags: ['events'],
         params: z.object({
           eventId: z.string().uuid(),
         }),
@@ -23,61 +25,44 @@ export const getEvent = async (app: FastifyInstance) => {
               totalAttendees: z.number().int(),
             }),
           }),
-          400: z.object({
-            message: z.string(),
-          }),
-          422: z.object({
-            error: z.string(),
-          }),
         },
       },
     },
     async (request, reply) => {
-      try {
-        const { eventId } = request.params
+      const { eventId } = request.params
 
-        const event = await prisma.event.findUnique({
-          select: {
-            id: true,
-            title: true,
-            slug: true,
-            details: true,
-            maximumAttendees: true,
-            _count: {
-              select: {
-                attendees: true,
-              },
+      const event = await prisma.event.findUnique({
+        select: {
+          id: true,
+          title: true,
+          slug: true,
+          details: true,
+          maximumAttendees: true,
+          _count: {
+            select: {
+              attendees: true,
             },
           },
-          where: {
-            id: eventId,
-          },
-        })
+        },
+        where: {
+          id: eventId,
+        },
+      })
 
-        if (event === null) {
-          return reply.status(400).send({
-            message: 'Event not found',
-          })
-        }
-
-        return reply.status(200).send({
-          event: {
-            id: event.id,
-            title: event.title,
-            slug: event.slug,
-            details: event.details,
-            maximumAttendees: event.maximumAttendees,
-            totalAttendees: event._count.attendees,
-          },
-        })
-      } catch (err) {
-        if (err instanceof ZodError) {
-          const validationError = fromZodError(err)
-          console.error('Error parsing request body:', validationError.message)
-          return reply.status(422).send({ error: validationError.message })
-        }
-        console.error(err)
+      if (event === null) {
+        throw new BadRequest('Event not found')
       }
+
+      return reply.status(200).send({
+        event: {
+          id: event.id,
+          title: event.title,
+          slug: event.slug,
+          details: event.details,
+          maximumAttendees: event.maximumAttendees,
+          totalAttendees: event._count.attendees,
+        },
+      })
     },
   )
 }
